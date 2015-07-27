6090e93cc05f5e0c732144cbb00cba2c23ad94cb
Sergey Romanov <xxsmotur@gmail.com>
2015-07-28 03:02:20 +0500

    first commit

---
+++ pyarxiv/pyarxiv.py
@@ -0,0 +1,39 @@
+import asyncio
+import requests
+
+#Implementation of Arxiv API
+#http://arxiv.org/help/api/index
+
+class PyArxiv:
+    def __init__(self):
+        self.loop = asyncio.get_event_loop()
+        self.commands = []
+
+    async def _get(self, path):
+        result = requests.get(path)
+        print(result.text)
+
+    def _add(self, func):
+        self.commands.append(asyncio.ensure_future(func))
+
+    def query(self, msg, start=0, max_items=10, sync=True):
+        path = 'http://export.arxiv.org/api/query?search_query=all:{0}&start={1}&max_results={2}'.format(msg, start, max_items)
+        if sync is False:
+            self.commands.append(asyncio.ensure_future(self._get(path)))
+        else:
+            r = requests.get(path)
+            if r.status_code == 200:
+                return r.text
+
+    def metadata(self, subset, startdate, enddate):
+        '''
+        Args:
+            subset - for example math, cs
+            startdate, enddate - must be in the format 2015-07-28
+        '''
+        path = 'http://export.arxiv.org/oai2?verb=ListRecords&set={0}&from={1}&until=2015-01-31&metadataPrefix=arXiv'
+        self.commands.append(asyncio.ensure_future(self._get(path)))
+
+    def run(self):
+        self.loop.run_until_complete(asyncio.wait(self.commands))
+        self.loop.close()
