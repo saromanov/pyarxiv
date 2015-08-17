0a1438a71c6b76f589e62c59a0ac99164412d3e2
Sergey Romanov <xxsmotur@gmail.com>
2015-08-17 23:47:16 +0500

    Add id_list for the query

--- pyarxiv/pyarxiv.py
+++ pyarxiv/pyarxiv.py
@@ -4,6 +4,9 @@ import requests
 #Implementation of Arxiv API
 #http://arxiv.org/help/api/index
 
+
+#http://academia.stackexchange.com/questions/38969/getting-a-dump-of-arxiv-metadata
+
 class PyArxiv:
     def __init__(self):
         self.loop = asyncio.get_event_loop()
@@ -16,8 +19,9 @@ class PyArxiv:
     def _add(self, func):
         self.commands.append(asyncio.ensure_future(func))
 
-    def query(self, msg, start=0, max_items=10, sync=True):
-        path = 'http://export.arxiv.org/api/query?search_query=all:{0}&start={1}&max_results={2}'.format(msg, start, max_items)
+    def query(self, msg, start=0, max_items=10, id_list=[], sync=True):
+        path = 'http://export.arxiv.org/api/query?search_query=all:{0}&start={1}&max_results={2}&id_list={3}'.format(msg, start, max_items,\
+                id_list)
         if sync is False:
             self.commands.append(asyncio.ensure_future(self._get(path)))
         else:
